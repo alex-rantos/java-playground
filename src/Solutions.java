955ca36cdc16bfc3806a0bdfb87e762c0ce54703
R-Alex95 <sdi1300147@di.uoa.gr>
2020-05-05 21:13:35 +0300

    leetMay

--- src/Solutions.java
+++ src/Solutions.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class Solutions {
@@ -7,7 +8,7 @@ public class Solutions {
     public static void main(String[] args) {
         
     }
-    
+    /* Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1. */
     public int firstUniqChar(String s) {
         int len = s.length();
         if (len <= 0) return -1;
@@ -28,4 +29,32 @@ public class Solutions {
         }
         return -1;
     }
+    public int firstUniqCharOptimal(String s){
+        int res = Integer.MAX_VALUE;
+        
+        for(char c = 'a'; c <= 'z'; c++){
+            int index = s.indexOf(c);
+            
+            if(index != -1 && index == s.lastIndexOf(c))
+                res = Math.min(res, index);
+        }
+        
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+
+    /* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.*/
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int remaining = target - nums[i];
+            if (map.containsKey(remaining)) {
+                return new int[]{map.get(remaining),i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{-1,-1};        
+    }
+
 }
--- test/SolutionsTest.java
+++ test/SolutionsTest.java
@@ -4,6 +4,7 @@ import src.Solutions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 
 public class SolutionsTest {
@@ -12,7 +13,7 @@ public class SolutionsTest {
 
     @BeforeEach
     public void setup() {
-        //System.out.println("initialized");
+        System.out.println("initialized".indexOf("i"));
         sol = new Solutions();
     }
 
@@ -21,6 +22,12 @@ public class SolutionsTest {
         assertEquals(1, sol.firstUniqChar("test"));
         assertEquals(0, sol.firstUniqChar("leetcode"));
         assertEquals(-1, sol.firstUniqChar("leetcodelcdto"));
+    }
+
+    @Test
+    public void twoSum() {
+        assertArrayEquals(new int[] {0,1}, sol.twoSum(new int[]{2, 7, 11, 15}, 9));
+        assertArrayEquals(new int[] {0,3}, sol.twoSum(new int[]{2, 7, 3, 24}, 26));
     }
 
 
